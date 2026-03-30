f46656a9ce8348bb1f6fab0157ed0d4ac29ebf9a
jnthnkabongo <jnthnkabongo@gmail.com>
2026-03-30 16:59:10 +0100

    Env

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
-Route::get('/', [ApiController::class, 'index'])->name('index');
+Route::get('/', [ApiController::class, 'index']);
 Route::post('/soumission-login', [ApiController::class, 'login'])->name('soumission-login');
 Route::post('/creation-compte', [ApiController::class, 'register'])->name('creation-compte');
 Route::get('/dashboard', [ApiController::class, 'dashboard'])->name('home');
